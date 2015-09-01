ecd09a8d14a84ec88574fd3342e977de63a44f31
perise <perise@gmail.com>
2015-09-01 14:40:44 +0800

    1.use WTF form to update the form 2.include check for form cell, server side check, client should also can handle this

--- myblog.py
+++ myblog.py
@@ -3,6 +3,8 @@
 from flask import Flask, render_template, request
 from StockInfo import StockInfo
 from flask_debugtoolbar import DebugToolbarExtension
+from wtforms import Form, StringField, validators
+import string
 
 app = Flask(__name__)
 # configure your application before initializing any extensions
@@ -17,7 +19,18 @@ toolbar = DebugToolbarExtension(app)
 def hello_world():
     return 'Hello My blog!'
 
-@app.route('/search/', methods=['get', 'post'])
+def stockcodecheck():
+    def _stockcodecheck(form, field):
+        if not str.isdigit(field.data.encode("ascii")) or len(field.data.encode("ascii")) is not 6:
+            raise validators.ValidationError('Field must equal to six digital number')
+    return _stockcodecheck
+
+class LoginForm(Form):
+    stockcode = StringField(u'股票代码', [validators.input_required(), stockcodecheck()])
+
+
+
+@app.route('/search', methods=['GET', 'POST'])
 def hello_good():
     titles = [u'股票名称',
         u'今开盘', u'昨收盘',
@@ -27,15 +40,15 @@ def hello_good():
         u'买一', u'买一价', u'买二', u'买二价', u'买三', u'买三价', u'买四', u'买四价', u'买五', u'买五价',
         u'卖一', u'卖一价', u'卖二', u'卖二价', u'卖三', u'卖三价', u'卖四', u'卖四价', u'卖五', u'卖五价',
         u'日期', u'时间']
-
-    if request.method == 'POST':
-        StockCode = request.form["StockCode"]
-        stock = StockInfo(StockCode)
+    form = LoginForm(request.form)
+    if request.method == 'POST' and form.validate():
+        stock = StockInfo(form.stockcode.data)
         contents = stock.getInfo().split(",")
         del contents[-1]
+
         if contents:
-            return render_template("index.html", titles=titles, contents=contents)
-    return render_template("index.html", titles=titles, contents=None)
+            return render_template("index.html", titles=titles, contents=contents, form=form)
+    return render_template("index.html", titles=titles, contents=None, form=form)
 
 if __name__ == '__main__':
     app.debug = True
